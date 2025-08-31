16270c11adfe33d064b81ae52247392e6cb54ca3
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-08-31 13:34:45 +0530

    Add src/api/controllers/userController.js

---
+++ src/api/controllers/userController.js
@@ -0,0 +1,44 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/User.js';
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.deleteOne();
+    res.json({ message: 'User removed' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  getUsers,
+  getUserById,
+  deleteUser,
+};
